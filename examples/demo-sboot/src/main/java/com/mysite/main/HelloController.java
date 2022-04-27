7471fc7265d7d5d98aa61523eb669d4fcb525230
itasworkshop <itasworkshop@gmail.com>
2022-04-27 15:00:13 +0530

    27 April 2

--- examples/demo-sboot/src/main/java/com/mysite/main/HelloController.java
+++ examples/demo-sboot/src/main/java/com/mysite/main/HelloController.java
@@ -1,16 +1,29 @@
 package com.mysite.main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@Controller
+@RestController //@controller + @ResponseBody
 public class HelloController {
 	
 	@RequestMapping("/")
 	@ResponseBody
 	String hello() {
 		return "Hello from my spring boot application!";
+	}
+	
+	@RequestMapping("/tools")
+	String myTools() {
+		return "Hello from my spring boot Tools application!";
+	}
+	
+	@RequestMapping("/students/{name}/{id}")
+	String myStudentIntro(@PathVariable String name,@PathVariable Long id) {
+		return "Hello this is "+name +" and my id is "+id;
 	}
 
 }
