c6072847b8010a98690766fca63a922ef19f9494
itasworkshop <itasworkshop@gmail.com>
2022-04-28 11:25:01 +0530

    28 April 1

--- examples/demo-sboot/src/main/java/com/mysite/main/DemoSbootApplication.java
+++ examples/demo-sboot/src/main/java/com/mysite/main/DemoSbootApplication.java
@@ -3,9 +3,12 @@ package com.mysite.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan("com.mysite.main")
+
 @SpringBootApplication
+@ComponentScan("com.mysite.main")
+@EnableJpaRepositories("com.mysite.main")
 public class DemoSbootApplication {
 
 	public static void main(String[] args) {
