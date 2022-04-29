eea216d038b9baf3a0aff507ed3140b25556d047
itasworkshop <itasworkshop@gmail.com>
2022-04-29 12:41:21 +0530

    29 April 1

--- examples/demo-sboot/src/main/java/com/mysite/main/MyUserController.java
+++ examples/demo-sboot/src/main/java/com/mysite/main/MyUserController.java
@@ -1,8 +1,13 @@
 package com.mysite.main;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +23,24 @@ public class MyUserController {
 	List<User> getAll(){
 		return userRepository.findAll();
 	}
+	
+	@GetMapping("/users/{id}")
+	Optional<User> getUserById(@PathVariable Long id){
+		return userRepository.findById(id);
+	}
+	
+	@PostMapping("/users")
+	User newUser(@RequestBody User user){
+		return userRepository.save(user);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	void deleteUserById(@PathVariable Long id){
+		userRepository.deleteById(id);
+	}
+	
+	
+	
+	
 
 }
